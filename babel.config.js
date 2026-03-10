aa57fb6f8fb0d7256cf99f2e8ceb9a3701107390
tikhercegovinaai-arch <tikhercegovina.a.i@gmail.com>
2026-03-10 02:41:06 +0100

    dsds

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      'react-native-reanimated/plugin',
       ['@babel/plugin-transform-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-methods', { loose: true }],
       ['@babel/plugin-transform-private-property-in-object', { loose: true }],
